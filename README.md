b7e41577c9cb663bdd17ed9df04f4a7848c22201
William-An <China_Aisa@live.com>
2019-02-07 16:38:43 -0500

    Finish GUI Prototype 2

--- README.md
+++ README.md
@@ -14,32 +14,14 @@ to operators' camera feed, including but not limited to the following:
 3. Enemies' positions on screen and off screen
 4. Aim scope
 
-![Preview of HUD Settiings](resource/images/preview.png)
+Model of HUD
+![Model of HUD](./resource/images/preview.png "Model Explain")
+GUI Design
+![GUI Design](./resource/images/GUI-Design.png "GUI Design")
+Current GUI Prototype
+![Current GUI Prototype](resource/images/GUI-Prototype-2.png)
 
 ## Todos
-<<<<<<< HEAD
-1. Preparation
-   1. [ ] Test display of LCD on Pi
-      1. ~~[Spidev](http://github.com/doceme/py-spidev)~~
-      2. [pygame](https://www.pygame.org/docs)
-      3. [pySerial](https://pyserial.readthedocs.io/en/latest/shortintro.html)
-   2. [ ] Determine serial communication data strcutre
-      1. Super Capacitor Booster
-      2. Kevin's special board :)
-      3. TX 2 / STM32 Maincontroller?
-2. Development
-   1. Super Capacitor
-      1. [ ] Boosting Strip display
-         1. [ ] RGB Support :)
-         2. [ ] User Friendly
-      2. [ ] Boosting time calculation and display
-      3. [ ] Voltage display
-   2. [ ] Sensor info
-   3. [ ] Enemy Identification
-      1. [ ] Box drawing on enemies
-      2. [ ] Triangles indication of enemies around
-   4. [ ] Aim scope
-=======
 
 1. Preparation
    1. Code
@@ -54,6 +36,7 @@ to operators' camera feed, including but not limited to the following:
    2. Hardware
       1. [x] Measure camera specs and installation position specs 
       2. [ ] LCD dimension measurement
+      3. [ ] Reflective Film Testing
 2. Development
    1. Code
       1. [ ] Display
@@ -81,4 +64,11 @@ to operators' camera feed, including but not limited to the following:
    2. Hardware
       1. [ ] Shell support design
       2. [ ] Shell printing and testing
->>>>>>> readme
+
+## Info
+1. The player display is 1920 x 1080 (16:9)
+2. The LCD panel is 480 x 320 pixels (3:2)
+3. Use the ratio 1080:320 ~= 2.5 for GUI development
+   1. Boost Strip
+      1. 25 px *  150 px
+      2. (255, 0, 0, 50)
--- main.py
+++ main.py
@@ -4,9 +4,28 @@ import pygame
 # Initialization
 pygame.init()
 
-# Constants Definitioin
+# Screen Constants
 RESOLUTION = WIDTH, HEIGHT = (480, 320)
+SCREEN_MARGIN = 30
 FRAME_RATE = 30
+
+# Boost Strip
+# Dimension of rectangle
+BOOSTSTRIP_WIDTH = 15
+BOOSTSTRIP_HEIGHT = 200
+BOOSTSTRIP_TOP = (HEIGHT + BOOSTSTRIP_HEIGHT) // 2
+BOOSTSTRIP_LEFT = WIDTH - SCREEN_MARGIN - BOOSTSTRIP_WIDTH
+
+# Strip configuration
+BOOSTSTRIP_COLOR = pygame.Color(150, 0, 0, 10)
+
+# Strip Frame
+BOOSTSTRIP_FRAME_WIDTH = 5
+BOOSTSTRIP_FRAME_COLOR = pygame.Color(255, 255, 255)
+BOOSTSTRIP_FRAME = pygame.Rect(BOOSTSTRIP_LEFT - BOOSTSTRIP_FRAME_WIDTH, 
+								BOOSTSTRIP_TOP + BOOSTSTRIP_FRAME_WIDTH,
+								BOOSTSTRIP_WIDTH + 2 * BOOSTSTRIP_FRAME_WIDTH, 
+								-(BOOSTSTRIP_HEIGHT + 2 * BOOSTSTRIP_FRAME_WIDTH))
 
 # Colors
 BLACK = (  0,   0,   0)
@@ -15,17 +34,73 @@ BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
 
+# Font
+class TextFormatter():
+	def __init__(self, font, textColor=(255, 255, 255)):
+		"""
+		TextFormatter Constructer
+
+		@param font: pygame.font.Font
+		@param textColor: color of the string
+		"""
+		self.font = font
+		self.textColor = textColor
+	
+	def format(self, formatString, startPos=(0, 0)):
+		"""
+		Create formatted text surface object
+
+		@param string: string contains new line character
+		@param startPos: Position of the final output text block, 
+						first is the right position, second is the bottom
+		@return textSurfaces: list of tuples containing textSurface object and its position
+		"""
+		strings = formatString.split("\n")
+		textSurfaces = []
+		startRight = startPos[0]
+		startBottom = startPos[1]
+		relativeBottom = 0
+		for string in strings:
+			textSurface, textSize = self._render(string)
+			textPos = (startRight - textSize[0], startBottom + relativeBottom)
+			textSurfaces.append((textSurface, textPos))
+			relativeBottom += textSize[1] 
+		return textSurfaces
+
+
+
+	def _render(self, string):
+		"""
+		Simple renderer wrapper
+
+		@param font: pygame.font.Font
+		@param string: string to put on screen
+		@return textSurface: pygame.Surface object of the string
+		@return textSize: size of text in pixels
+		"""
+		textSurface = self.font.render(string, False, self.textColor)
+		textSize = self.font.size(string)
+
+		return textSurface, textSize
+
+textfont = pygame.font.Font('./resource/fonts/RobotSlab/RobotoSlab-Thin.ttf', 15)
+textFormatter = TextFormatter(textfont, WHITE)
+
+# Buckle Up!
 screen = pygame.display.set_mode(RESOLUTION)
+
 running = True
 clock = pygame.time.Clock()
-
 capVolt = 0
 
 while running:
 	# Run loop at most FRAME_RATE per seconds
 	clock.tick(FRAME_RATE)
 
-	# Get events
+	#--------------------------------
+	# Event handling
+	#--------------------------------
+	# TODO: Read from Serial and decode data comm
 	for event in pygame.event.get():
 		# only do something if the event is of type QUIT
 		if event.type == pygame.QUIT:
@@ -39,29 +114,48 @@ while running:
 				# Decrease capacitor voltage
 				capVolt -= 1
 
+	#--------------------------------
+	# Super Capacitor Booster Display
+	#--------------------------------
+
 	# Correct value
-	capVolt = 10 if capVolt > 10 else capVolt
+	capVolt = 20 if capVolt > 20 else capVolt
 	capVolt = 0 if capVolt < 0 else capVolt
+
+	# Create new strip
+	# -1 * height as the coordinate system is inverted
+	boostStrip = pygame.Rect(BOOSTSTRIP_LEFT, BOOSTSTRIP_TOP, 
+							BOOSTSTRIP_WIDTH, -capVolt * BOOSTSTRIP_HEIGHT // 20)
+
+	#--------------------------------
+	# Text Display
+	#--------------------------------
 
 	# Text info display
 	fps = clock.get_fps()
 	time = clock.get_rawtime()
-	formatStr = "FPS: %.2f | Time: %.2f" % (fps, time / 1000)
-	capDisplay = "[%-10s]" % (capVolt * "#")
+
+	sensorData = "FPS: %.2f\nTime: %.2f\nCapVolt: %.2f" % (fps, time / 1000, capVolt)
 
 	# Text renderers
-	textfont = pygame.font.Font('./fonts/CursedTimerULiL.ttf', 20)
-	boostfont = pygame.font.Font('./fonts/CursedTimerULiL.ttf', 20)
+	# Align to the strip
+	textSurfaces = textFormatter.format(sensorData, (WIDTH - SCREEN_MARGIN - BOOSTSTRIP_WIDTH - 10, (HEIGHT - BOOSTSTRIP_HEIGHT) // 2))
 
-	# Render
-	textsurface = textfont.render(formatStr, False, WHITE)
-	boostStrip = boostfont.render(capDisplay, False, WHITE)
 
-	# Update screen		
+	#--------------------------------
+	# Screen Update
+	#--------------------------------
+	
 	screen.fill(BLACK)
-	textPos = textfont.size(formatStr)[0]  # Get the width of string in pixels
-	screen.blit(textsurface, (WIDTH - textPos, 0))
-	screen.blit(boostStrip, (0, HEIGHT // 2))
+	
+	# Text update
+	for surface, pos in textSurfaces:
+		screen.blit(surface, pos)
+
+	# Boost strip update
+	pygame.draw.rect(screen, BOOSTSTRIP_FRAME_COLOR, BOOSTSTRIP_FRAME, BOOSTSTRIP_FRAME_WIDTH)  # Draw Frame
+	pygame.draw.rect(screen, BOOSTSTRIP_COLOR, boostStrip, 0)  # Draw actual boost strip
+
 	pygame.display.update()
 
 pygame.quit()
