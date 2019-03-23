01d8f42695b52e1f8f4a6bd4216fb6191a27fee7
William-An <China_Aisa@live.com>
2019-03-23 00:51:27 -0400

    1. Update README 2. Use threading serial reader and queue to ensure high performance 3. Pack configuration statements to util.py file 4. Add new prototype images 5. Add shell design stl files

--- README.md
+++ README.md
@@ -14,23 +14,43 @@ to operators' camera feed, including but not limited to the following:
 3. Enemies' positions on screen and off screen
 4. Aim scope
 
+
+Current GUI Prototype
+![Current GUI Prototype](./resource/images/GUI-Prototype-3.png)
+Current Hardware design
+![Current Hardware design](./resource/images/HUD-Shell-V2.png)
 Model of HUD
 ![Model of HUD](./resource/images/preview.png "Model Explain")
 GUI Design
 ![GUI Design](./resource/images/GUI-Design.png "GUI Design")
-Current GUI Prototype
-![Current GUI Prototype](resource/images/GUI-Prototype-2.png)
 
 ## Maintainers
 
 [William](https://purduerobomaster.slack.com/team/UCNH7S52P)
 ![Headshot](resource/doc/Weili-An.png)
 
-## Todos
+## Version 2 Update
+
+1. Code
+    1. Performance
+        1. 30+ fps on Macbook Pro 2017
+    1. [ ] Data Communication
+        1. Threading Reading
+            1. [x] Use another thread for serial data reading
+            1. Use Lifoqueue to store serial data and ensure data validity
+            1. Extend Packetizer class in pyserial for customized data communication
+                1. Possibly C struct or object stream [Ref](https://docs.python.org/3/library/struct.html)
+        1. C Struct packing and unpacking
+            1. Use C Struct to transmit data between components
+2. Hardware
+    1. Mainly use triangles to improve structure strength
+    1. Use 5mm organic glass to protect reflective film
+
+## Version 1
 
 1. Preparation
    1. Code
-      1. [ ] Test display of LCD on Pi
+      1. [x] Test display of LCD on Pi
          1. ~~[Spidev](http://github.com/doceme/py-spidev)~~
          2. [pygame](https://www.pygame.org/docs)
          3. [pySerial](https://pyserial.readthedocs.io/en/latest/shortintro.html)
@@ -40,39 +60,37 @@ Current GUI Prototype
          3. [ ] TX 2 / STM32 Maincontroller?
    2. Hardware
       1. [x] Measure camera specs and installation position specs 
-      2. [ ] LCD dimension measurement
-      3. [ ] Reflective Film Testing
+      2. [x] LCD dimension measurement
+      3. [x] Reflective Film Testing
 2. Development
    1. Code
-      1. [ ] Display
+      1. [x] Display
          1. [ ] Super Capacitor
-            1. [ ] Boosting Strip display
-               1. [ ] RGB Support :)
-               2. [ ] User Friendly
-            2. [ ] Boosting time calculation and display
-            3. [ ] Voltage display
-         2. [ ] Sensor info
+            1. Boosting Strip display
+            2. Boosting time calculation and display
+            3. Voltage display
+         2. [x] Sensor info
          3. [ ] Enemy Identification
-            1. [ ] Box drawing on enemies
-            2. [ ] Triangles indication of enemies around
+            1. Box drawing on enemies
+            2. Triangles indication of enemies around
          4. [ ] Aim scope
-      2. [ ] Data communication
+      2. [x] Data communication
          1. [ ] Serial Communication Class
          2. [ ] [Super Capacitor Booster embedded system](https://github.com/RoboMaster-Club/Super-Capacitor-Booster)
             1. [ ] Raw data reading
             2. [ ] Data processing
             3. [ ] Better transmission speed
-               1. [ ] Serialize object? between python and c
-               2. [ ] Rewrite in C
+               1. Serialize object? between python and c
+               2. ~~Rewrite in C~~
          3. [ ] Custom Power distribution Unit
-            1. [ ] Raw data reading
-            2. [ ] Data processing
+            1. Raw data reading
+            2. Data processing
          4. [ ] TX 2 / STM32 Maincontroller
-            1. [ ] Raw data reading
-            2. [ ] Data processing
+            1. Raw data reading
+            2. Data processing
    2. Hardware
-      1. [ ] Shell support design
-      2. [ ] Shell printing and testing
+      1. [x] Shell support design
+      2. [x] Shell printing and testing
 
 ## Info
 1. The player display is 1920 x 1080 (16:9)
--- main.py
+++ main.py
@@ -1,143 +1,58 @@
 #!/usr/bin/env python
 import pygame
 import serial
+from serial.threaded import ReaderThread
+from queue import LifoQueue, Full, Empty
 import os
+import sys
+import platform
+from util import *
 
 # Initialization
-drivers = ['fbcon', 'directfb', 'svgalib']
-found = False
-for driver in drivers:
-	# Make sure that SDL_VIDEODRIVER is set
-	if not os.getenv('SDL_VIDEODRIVER'):
-		os.putenv('SDL_VIDEODRIVER', driver)
-	try:
-		pygame.display.init()
-	except pygame.error:
-		print('Driver: %s failed.' % driver)
-		continue
-	found = True
-	break
+system = platform.system()
+if system != 'Darwin':
+	drivers = ['fbcon', 'directfb', 'svgalib']
+	found = False
+	for driver in drivers:
+		# Make sure that SDL_VIDEODRIVER is set
+		if not os.getenv('SDL_VIDEODRIVER'):
+			os.putenv('SDL_VIDEODRIVER', driver)
+		try:
+			pygame.display.init()
+		except pygame.error:
+			print('Driver: %s failed.' % driver)
+			continue
+		found = True
+		break
 
-if not found:
-	raise Exception('No suitable video driver found!')
+	if not found:
+		raise Exception('No suitable video driver found!')
+
+pygame.display.init()
 pygame.font.init()
 
-# Screen Constants
-RESOLUTION = WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
-SCREEN_MARGIN = 30
-FRAME_RATE = 40
-
-# Colors
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
-
-# Boost Strip
-# Dimension of rectangle
-BOOSTSTRIP_WIDTH = 15
-BOOSTSTRIP_HEIGHT = 200
-BOOSTSTRIP_TOP = (HEIGHT + BOOSTSTRIP_HEIGHT) // 2
-BOOSTSTRIP_LEFT = WIDTH - SCREEN_MARGIN - BOOSTSTRIP_WIDTH
-
-# Strip configuration
-BOOSTSTRIP_COLOR = WHITE
-
-# Strip Frame
-BOOSTSTRIP_FRAME_WIDTH = 5
-BOOSTSTRIP_FRAME_COLOR = pygame.Color(255, 255, 255)
-BOOSTSTRIP_FRAME = pygame.Rect(BOOSTSTRIP_LEFT - BOOSTSTRIP_FRAME_WIDTH, 
-								BOOSTSTRIP_TOP + BOOSTSTRIP_FRAME_WIDTH,
-								BOOSTSTRIP_WIDTH + 2 * BOOSTSTRIP_FRAME_WIDTH, 
-								-(BOOSTSTRIP_HEIGHT + 2 * BOOSTSTRIP_FRAME_WIDTH))
-
-# Font
-class TextFormatter():
-	def __init__(self, font, textColor=(255, 255, 255)):
-		"""
-		TextFormatter Constructer
-
-		@param font: pygame.font.Font
-		@param textColor: color of the string
-		"""
-		self.font = font
-		self.textColor = textColor
-	
-	def format(self, formatString, startPos=(0, 0)):
-		"""
-		Create formatted text surface object
-
-		@param string: string contains new line character
-		@param startPos: Position of the final output text block, 
-						first is the right position, second is the bottom
-		@return textSurfaces: list of tuples containing textSurface object and its position
-		"""
-		strings = formatString.split("\n")
-		textSurfaces = []
-		startRight = startPos[0]
-		startBottom = startPos[1]
-		relativeBottom = 0
-		for string in strings:
-			textSurface, textSize = self._render(string)
-			textPos = (startRight - textSize[0], startBottom + relativeBottom)
-			textSurfaces.append((textSurface, textPos))
-			relativeBottom += textSize[1] 
-		return textSurfaces
-
-
-
-	def _render(self, string):
-		"""
-		Simple renderer wrapper
-
-		@param font: pygame.font.Font
-		@param string: string to put on screen
-		@return textSurface: pygame.Surface object of the string
-		@return textSize: size of text in pixels
-		"""
-		textSurface = self.font.render(string, False, self.textColor)
-		textSize = self.font.size(string)
-
-		return textSurface, textSize
-
-class SerialWrapper(serial.Serial):
-	"""
-	Dummy Class for wrapping serial communication and parsing data
-	"""
-	def __init__(self):
-		pass
-	def read(self):
-		pass
-	def write(self):
-		pass
-
-class SerialDataObject():
-	pass
-
+# Text
 textfont = pygame.font.Font('./resource/fonts/RobotSlab/RobotoSlab-Thin.ttf', 15)
 textFormatter = TextFormatter(textfont, WHITE)
 
 # Serial Configuration
-delimiter = ','
 ser = serial.Serial()
-ser.port = '/dev/cu.usbserial-00000000'
-ser.timeout = 0.01  # Reading Timeout is 10 ms
-ser.baudrate = 19200
+ser.port = '/dev/cu.usbserial-00000000' if system == 'Darwin' else '/dev/ttyAMA0'
+ser.baudrate = 115200
 ser.parity = serial.PARITY_EVEN
-maxTrials = 5
-
-# Data communication
-MaxCapVolt = 24
-
-# Buckle Up!
-screen = pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE)
 ser.open()
-ser.reset_input_buffer()
+
+# Start serial data thread
+serialThread = ReaderThread(ser, ReadData)
+serialThread.start()
+
+# Start screen
+displayMode = not pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE if system == 'Darwin' else pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE
+screen = pygame.display.set_mode(RESOLUTION, displayMode)
 
 running = True
 clock = pygame.time.Clock()
-capVolt = 0
+
 while running:
 	# Run loop at most FRAME_RATE per seconds
 	clock.tick(FRAME_RATE)
@@ -152,37 +67,18 @@ while running:
 			if event.key == 27:
 				running = False
 
-	# TODO: Read from Serial and decode data communication
-	# Use comma for separation
-	data = None
-	capVolt = 0
-	tried = 0
+	#--------------------------------
+	# Serial data reading (blocking)
+	#--------------------------------
+	# TODO Use C struct
+	data = bufferQueue.get()
+	data = data.strip()
+	data = data.split(delimiter)
+	n = len(data)
+	data = [(data[i], data[i + 1]) for i in range(0, n, 2)]
+	data = dict(data)
+	capVolt = eval(data.pop('capVolt'))
 
-	# Ensure getting the realtime data
-	while tried < maxTrials:
-		tried += 1
-		data = ser.readline().decode().strip()
-		print(data)
-		if not data:
-			continue 
-		# Validate data
-		data = data.split(',')
-		n = len(data)
-		# Acquire checksum
-		print(data)
-		try:
-			data = [(data[i], data[i + 1]) for i in range(0, n, 2)] 
-			data = dict(data)
-			capVolt = eval(data.pop('capVolt'))
-			print(data)
-			break
-		except BaseException:
-			continue
-	else:
-		capVolt = 0
-		data = {"Communcation": "Failed"}
-
-	
 
 	#--------------------------------
 	# Super Capacitor Booster Display
@@ -190,7 +86,7 @@ while running:
 
 	# Create new strip
 	# -1 * height as the coordinate system is inverted
-	boostStrip = pygame.Rect(BOOSTSTRIP_LEFT, BOOSTSTRIP_TOP, 
+	boostStrip = pygame.Rect(BOOSTSTRIP_LEFT, BOOSTSTRIP_TOP,
 							BOOSTSTRIP_WIDTH, -capVolt * BOOSTSTRIP_HEIGHT // MaxCapVolt)
 
 	#--------------------------------
@@ -204,15 +100,16 @@ while running:
 
 	# Text renderers
 	# Align to the strip
-	textSurfaces = textFormatter.format(sensorData, (WIDTH - SCREEN_MARGIN - BOOSTSTRIP_WIDTH - 10, (HEIGHT - BOOSTSTRIP_HEIGHT) // 2))
-
+	textSurfaces = textFormatter.format(sensorData,
+										(WIDTH - SCREEN_MARGIN - BOOSTSTRIP_WIDTH - 10,
+										(HEIGHT - BOOSTSTRIP_HEIGHT) // 2))
 
 	#--------------------------------
 	# Screen Update
 	#--------------------------------
-	
+
 	screen.fill(BLACK)
-	
+
 	# Text update
 	for surface, pos in textSurfaces:
 		screen.blit(surface, pos)
@@ -225,5 +122,5 @@ while running:
 	flipped = pygame.transform.flip(screen, False, True)
 	# screen.blit(flipped, (0, 0))
 	pygame.display.update()
-ser.close()
+serialThread.close()
 pygame.quit()
